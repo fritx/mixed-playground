88c0d96622bb388391c9133847b52472fdb1c0e1
Fritz Lin <uxfritz@163.com>
2024-02-25 14:14:00 +0800

    python架构图生成

---
+++ diagrams/01. Web Service.py
@@ -0,0 +1,9 @@
+# https://diagrams.mingrammer.com/docs/getting-started/installation#quick-start
+
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+
+with Diagram("Web Service", show=False):
+    ELB("lb") >> EC2("web") >> RDS("userdb")
---
+++ diagrams/README.md
@@ -0,0 +1,9 @@
+```sh
+# Install Dependencies
+pip install -r requirements.txt
+
+# Generate Images
+python "01. Web Service.py"
+```
+
+- https://diagrams.mingrammer.com/docs/getting-started/examples
